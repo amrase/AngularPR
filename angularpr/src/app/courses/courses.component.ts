e5fdcd4d74988c0be0e282dbc7b9908bceded515
unknown <amrasezairi31@gmail.com>
2021-01-15 00:25:45 -0500

    handling events

--- angularpr/src/app/courses/courses.component.ts
+++ angularpr/src/app/courses/courses.component.ts
@@ -8,14 +8,46 @@ import { CoursesService } from './courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  title = "List of courses";
-  courses:any[];
+  // isActive=true;
+  // title = "List of courses";
+   courses:any[];
+  // imageUrl="";
+
+  // colsapan=100;
 
   constructor(private courseService:CoursesService) { }
+
+
+  // onKeyUp($event){
+  //   if($event.keyCode === 13) console.log("Enter was pressed");
+    
+  // }
+
+  // name:string;
+
+  // onKeyUp($event){
+  //   console.log("enter was pressed",$event.target.value);
+  // }
+  // onSave($event){
+  //   $event.stopPropagation();
+  //   console.log("Button clicked",$event);
+  // }
+
+  // emailUp(email){
+  //   console.log("Email",email)
+  // }
+
+  // onDivClicked(){
+  //   console.log("Div clicked");
+  // }
 
 
   ngOnInit(): void {
     this.courses = this.courseService.getCourses();
   }
 
+
+  // onKeyUpPress(){
+  //   console.log("Name",this.name);
+  // }
 }
